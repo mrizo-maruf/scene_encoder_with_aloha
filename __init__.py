277ae30ced53fb1baaf1a03265a36312c5da8f57
Nikita <xisoniki@gmail.com>
2024-11-18 18:20:25 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from gymnasium.envs.registration import register
-print("register")
-
-register(
-     id="AlphaBaseEnv-v0",
-     entry_point="tasks.env_n:AlphaBaseEnv",
-     max_episode_steps=2700,
-)
